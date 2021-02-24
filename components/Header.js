b1d97d4d949a99093882eb271ae7e63dd851fb83
Shikhar15606 <shikharshukla15606@gmail.com>
2021-02-24 11:10:31 +0530

    Moving styles out of the fxnl component

--- components/Header.js
+++ components/Header.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const Header = props => {
-    const styles = StyleSheet.create({
-        headerBar: {
-            backgroundColor: "#7C4DFF",
-            width: "100%",
-            height: 90,
-            paddingTop: 36,
-            alignItems: "center",
-        },
-        headerText: {
-            color: "#FFFFFF",
-            fontSize: 20
-        }
-    })
     return (
         <View style={styles.headerBar} >
             <Text style={styles.headerText}>{props.title}</Text>
         </View>
     );
-
 };
+
+const styles = StyleSheet.create({
+    headerBar: {
+        backgroundColor: "#7C4DFF",
+        width: "100%",
+        height: 90,
+        paddingTop: 36,
+        alignItems: "center",
+    },
+    headerText: {
+        color: "#FFFFFF",
+        fontSize: 20
+    }
+})
 
 export default Header;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,28 +3,6 @@ import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import Card from '../components/Card';
 
 const StartGameScreen = () => {
-    const styles = StyleSheet.create({
-        screen: {
-            flex: 1,
-            padding: 10,
-            alignItems: "center"
-        },
-        title: {
-            fontSize: 20,
-            marginVertical: 10,
-        },
-        inputContainer: {
-            width: 300,
-            maxWidth: "80%",
-            alignItems: "center"
-        },
-        buttonContainer: {
-            flexDirection: 'row',
-            width: "100%",
-            justifyContent: "space-between",
-            paddingHorizontal: 15
-        }
-    })
     return (
         <View style={styles.screen}>
             <Text style={styles.title}>Start a new Game!</Text>
@@ -39,5 +17,28 @@ const StartGameScreen = () => {
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 10,
+        alignItems: "center"
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10,
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: "80%",
+        alignItems: "center"
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: "100%",
+        justifyContent: "space-between",
+        paddingHorizontal: 15
+    }
+})
 
 export default StartGameScreen;
