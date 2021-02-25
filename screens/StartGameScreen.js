c271ffd1bea063046df4e829f3aaf3a34356c952
Shikhar15606 <shikharshukla15606@gmail.com>
2021-02-25 17:21:03 +0530

    Reusable Text Input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
 import Card from '../components/Card';
+import Input from '../components/Input';
+
 import Colors from '../constants/colors';
 
 const StartGameScreen = () => {
@@ -9,7 +12,7 @@ const StartGameScreen = () => {
             <Text style={styles.title}>Start a new Game!</Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <TextInput />
+                <Input style={styles.input} blurOnSubmit autoCapitalize="none" keyboardType="number-pad" autoCorrect={false} maxLength={2}/>
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
                         <Button title="Reset" onPress={() => { }} color={Colors.primary}></Button>
@@ -46,6 +49,10 @@ const styles = StyleSheet.create({
     },
     button: {
         width: 100
+    },
+    input:{
+        width:50,
+        textAlign:'center'
     }
 })
 
