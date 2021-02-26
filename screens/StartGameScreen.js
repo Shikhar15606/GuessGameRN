9074907efd9de060ea2937e8e8729463ccba6efe
Shikhar15606 <shikharshukla15606@gmail.com>
2021-02-26 20:30:10 +0530

    Number Container

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Ale
 
 import Card from '../components/Card';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
 
 import Colors from '../constants/colors';
 
@@ -30,13 +31,20 @@ const StartGameScreen = () => {
         setenteredValue('')
         setconfirmed(true)
         setselectedNumber(chosenNumber)
+        Keyboard.dismiss()
         // this will be batched by react
     }
 
     let confirmedOutput
 
     if(confirmed){
-        confirmedOutput = <Text>Chosen Number : {selectedNumber}</Text>
+        confirmedOutput = (
+        <Card style={styles.summaryContainer}>
+            <Text>You selected</Text>
+            <NumberContainer>{selectedNumber}</NumberContainer>
+            <Button title="START GAME" />
+        </Card>
+        )
     }
 
     return (
@@ -88,6 +96,10 @@ const styles = StyleSheet.create({
     input:{
         width:50,
         textAlign:'center'
+    },
+    summaryContainer:{
+        marginTop:20,
+        alignItems:'center'
     }
 })
 
