2645c78df127d209b68f920f464662c70635ebc9
Shikhar15606 <shikharshukla15606@gmail.com>
2021-03-14 02:09:09 +0530

    Playing with Image Assets

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Image } from 'react-native';
 
 import TitleText from '../components/TitleText';
 import BodyText from '../components/BodyText';
@@ -7,7 +7,14 @@ import BodyText from '../components/BodyText';
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
+      <TitleText style={styles.titleText}>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          resizeMode='cover'
+          source={require('../assets/success.png')}
+        />
+      </View>
       <BodyText>
         The Computer Gussed your number in : {props.guessRounds} gusses
       </BodyText>
@@ -22,6 +29,22 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderColor: 'black',
+    borderWidth: 3,
+    borderRadius: 150,
+    overflow: 'hidden',
+    marginVertical: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  titleText: {
+    fontSize: 20,
   },
 });
 
